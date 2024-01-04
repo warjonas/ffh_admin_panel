8893bb756cd7598b19c847ac32225b3e89c8fca3
Warren Jonas <jonaswarren9@gmail.com>
2024-01-04 23:36:36 +0200

    prisma seed updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,26 +3,24 @@ import { PrismaClient } from '@prisma/client';
 const prismadb = new PrismaClient();
 
 async function main() {
-  await prismadb.coffin.createMany({
-    data: [
-      {
-        name: 'Flat Lid',
-        price: 1200,
-      },
-      {
-        name: '3 tier coffin',
-        price: 2500,
-      },
-
-      {
-        name: 'Pongee Casket',
-        price: 5000,
-      },
-    ],
-  });
-
-  const coffins = await prismadb.coffin.findMany({});
-  console.dir(coffins, { depth: null });
+  // await prismadb.coffin.createMany({
+  //   data: [
+  //     {
+  //       name: 'Flat Lid',
+  //       price: 1200,
+  //     },
+  //     {
+  //       name: '3 tier coffin',
+  //       price: 2500,
+  //     },
+  //     {
+  //       name: 'Pongee Casket',
+  //       price: 5000,
+  //     },
+  //   ],
+  // });
+  // const coffins = await prismadb.coffin.findMany({});
+  // console.dir(coffins, { depth: null });
 }
 
 main()
