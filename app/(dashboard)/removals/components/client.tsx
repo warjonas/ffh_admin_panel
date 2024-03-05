4b964658abe41af5922c0840671b0564cab9bd2c
Warren Jonas <jonaswarren9@gmail.com>
2024-03-05 10:29:45 +0200

    🔥cleaned up removal client component and removed receipt (v1) modal

--- app/(dashboard)/removals/components/client.tsx
+++ app/(dashboard)/removals/components/client.tsx
@@ -1,55 +1,16 @@
 'use client';
 
-import { useParams, useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import React from 'react';
 import { BodyRemovalColumn, columns } from './columns';
 import { DataTable } from '@/components/ui/data-table';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover';
-import { Button } from '@/components/ui/button';
-import { Check, ChevronsUpDown, StoreIcon } from 'lucide-react';
-import { cn } from '@/lib/utils';
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-} from '@/components/ui/command';
 
 interface BodyRemovalClientProps {
   data: BodyRemovalColumn[];
 }
 
-const items = [
-  {
-    id: 1,
-    label: 'Invoice',
-    value: 'invoice',
-  },
-  {
-    id: 2,
-    label: 'First Name',
-    value: 'firstName',
-  },
-  {
-    id: 3,
-    label: 'Member No.',
-    value: 'memberNo',
-  },
-];
-
 export const BodyRemovalClient: React.FC<BodyRemovalClientProps> = ({
   data,
 }) => {
-  const router = useRouter();
-  const params = useParams();
-
   return (
     <>
       <DataTable columns={columns} data={data} searchKey="name" />
